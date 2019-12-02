d2c59a5080fd88652026890db683c81323b7429f
Saladin <negaton.app@gmail.com>
2019-12-02 15:34:23 +0700

    test

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+
+/* @author Saladin */
+/* NONAME FRAMEWORK BUILD BY ME */
+date_default_timezone_set("Asia/Bangkok");
+require './core/loader.php';
+$uriSegments = explode("/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+if (count($uriSegments) >= 4) {
+    $page = './page/' . $uriSegments[3] . '.php';
+    if (file_exists($page)) {
+        require $page;
+    } else {
+        echo 'No Page';
+    }
+} else {
+    header('location:' . $ROUTE['BASEURL'] . $ROUTE['HOME']);
+}
